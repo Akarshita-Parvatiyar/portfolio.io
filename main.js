ce33b44da3cca7cd6b8cc8542d42bf66bd2618f1
akarshita <ap6353@srmist.edu.in>
2020-12-25 17:32:27 +0530

    commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const bodyelement = document.querySelector('body');
+const mediatoggle = document.querySelector('.hamburger-menu');
+const heroelement = document.querySelector('.hero');
+const navList = document.querySelector('.nav-list');
+
+mediatoggle.addEventListener('click',function(){
+    bodyelement.classList.toggle('is-open');
+    heroelement.classList.add('is-open');
+});
+
+navList.addEventListener('click',function(e){
+     let clickedElement = e.target;
+
+     if(clickedElement.classList[0]==="nav-link"){
+         bodyelement.classList.remove('is-open');
+     }
+});
+
+Window.addEventListener('click',function(e){
+    let clickedElement = e.target;
+    if(clickedElement.matches('.is-open')){
+        bodyelement.classList.remove('is-open');
+        heroelement.classList.remove('is-open');
+    }
+
+});
